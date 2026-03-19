afb805b33c2d8f104a1541dfac9f8f0157fb1431
Luana Cabral <163360766+luanacabral05@users.noreply.github.com>
2026-03-19 22:18:39 +0000

    Refatoração do models

--- express-01/api/index.js
+++ express-01/api/index.js
@@ -3,36 +3,17 @@ import cors from "cors";
 import express from "express";
 import { v4 as uuidv4 } from "uuid";
 
-let users = {
-  1: {
-    id: "1",
-    username: "Robin Wieruch",
-  },
-  2: {
-    id: "2",
-    username: "Dave Davids",
-  },
-};
-
-let messages = {
-  1: {
-    id: "1",
-    text: "Hello World",
-    userId: "1",
-  },
-  2: {
-    id: "2",
-    text: "By World",
-    userId: "2",
-  },
-};
+import models from "./models";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  req.me = users[1];
+  req.context = {
+    models,
+    me: models.users[1],
+  };
   next();
 });
 
@@ -55,11 +36,11 @@ app.delete("/", (req, res) => {
 });
 
 app.get("/users", (req, res) => {
-  return res.send(Object.values(users));
+  return res.send(Object.values(req.context.models.users));
 });
 
 app.get("/users/:userId", (req, res) => {
-  return res.send(users[req.params.userId]);
+  return res.send(req.context.models.users[req.params.userId]);
 });
 
 app.post("/users", (req, res) => {
@@ -75,11 +56,11 @@ app.delete("/users/:userId", (req, res) => {
 });
 
 app.get("/messages", (req, res) => {
-  return res.send(Object.values(messages));
+  return res.send(Object.values(req.context.models.messages));
 });
 
 app.get("/messages/:messageId", (req, res) => {
-  return res.send(messages[req.params.messageId]);
+  return res.send(req.context.models.messages[req.params.messageId]);
 });
 
 app.post("/messages", (req, res) => {
@@ -87,24 +68,25 @@ app.post("/messages", (req, res) => {
   const message = {
     id,
     text: req.body.text,
-    userId: req.me.id,
+    userId: req.context.me.id,
   };
 
-  messages[id] = message;
+  req.context.models.messages[id] = message;
 
   return res.send(message);
 });
 
 app.delete("/messages/:messageId", (req, res) => {
-  const { [req.params.messageId]: message, ...otherMessages } = messages;
+  const { [req.params.messageId]: message, ...otherMessages } =
+    req.context.models.messages;
 
-  messages = otherMessages;
+  req.context.models.messages = otherMessages;
 
   return res.send(message);
 });
 
 app.get("/session", (req, res) => {
-  return res.send(users[req.me.id]);
+  return res.send(req.context.models.users[req.context.me.id]);
 });
 
 const port = process.env.PORT ?? 3000;
