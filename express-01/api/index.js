46c0ad850d4d806151b8ba9b9b97b1fd312eda6b
Luana Cabral <163360766+luanacabral05@users.noreply.github.com>
2026-03-19 22:15:11 +0000

    Métodos http para users e messages

--- express-01/api/index.js
+++ express-01/api/index.js
@@ -2,6 +2,30 @@ import "dotenv/config";
 import cors from "cors";
 import express from "express";
 
+let users = {
+  1: {
+    id: "1",
+    username: "Robin Wieruch",
+  },
+  2: {
+    id: "2",
+    username: "Dave Davids",
+  },
+};
+
+let messages = {
+  1: {
+    id: "1",
+    text: "Hello World",
+    userId: "1",
+  },
+  2: {
+    id: "2",
+    text: "By World",
+    userId: "2",
+  },
+};
+
 const app = express();
 app.use(cors());
 
@@ -9,6 +33,46 @@ app.get("/", (req, res) => {
   res.send(
     "Received a GET HTTP method\nServidor rodando!\n" + process.env.MESSAGE,
   );
+});
+
+app.post("/", (req, res) => {
+  return res.send("Received a POST HTTP method");
+});
+
+app.put("/", (req, res) => {
+  return res.send("Received a PUT HTTP method");
+});
+
+app.delete("/", (req, res) => {
+  return res.send("Received a DELETE HTTP method");
+});
+
+app.get("/users", (req, res) => {
+  return res.send(Object.values(users));
+});
+
+app.get("/users/:userId", (req, res) => {
+  return res.send(users[req.params.userId]);
+});
+
+app.post("/users", (req, res) => {
+  return res.send("POST HTTP method on user resource");
+});
+
+app.put("/users/:userId", (req, res) => {
+  return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
+});
+
+app.delete("/users/:userId", (req, res) => {
+  return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
+});
+
+app.get("/messages", (req, res) => {
+  return res.send(Object.values(messages));
+});
+
+app.get("/messages/:messageId", (req, res) => {
+  return res.send(messages[req.params.messageId]);
 });
 
 const port = process.env.PORT ?? 3000;
