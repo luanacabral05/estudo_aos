dceda7dc2f330b22437f23145f52f2c93b0f361f
Luana Cabral <163360766+luanacabral05@users.noreply.github.com>
2026-03-19 22:21:06 +0000

    Log com o IP real, ao invés do IP local

--- express-01/api/index.js
+++ express-01/api/index.js
@@ -6,6 +6,7 @@ import models from "./models";
 import routes from "./routes";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
