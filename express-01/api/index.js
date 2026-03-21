6f1a236cd3437c481e201773476382b54ec96055
Luana Cabral <163360766+luanacabral05@users.noreply.github.com>
2026-03-21 18:31:00 +0000

    Atualizadas as rotas para utilizar o sequelize

--- express-01/api/index.js
+++ express-01/api/index.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import cors from "cors";
 import express from "express";
 
-import models from "./models";
+import models, { sequelize } from "./models";
 import routes from "./routes";
 
 const app = express();
@@ -10,10 +10,10 @@ app.set("trust proxy", true);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
+app.use(async (req, res, next) => {
   req.context = {
     models,
-    me: models.users[1],
+    me: await models.User.findByLogin("rwieruch"),
   };
   next();
 });
@@ -33,11 +33,53 @@ app.get("/", (req, res) => {
 });
 
 const port = process.env.PORT ?? 3000;
+const eraseDatabaseOnSync = process.env.ERASE_DATABASE_ON_SYNC === "true";
 
-app.listen(port, () =>
-  console.log(
-    "Express-01 app listening on port " + port + "!\n" + process.env.MESSAGE,
-  ),
-);
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  if (eraseDatabaseOnSync) {
+    createUsersWithMessages();
+  }
+
+  app.listen(port, () =>
+    console.log(
+      "Express-01 app listening on port " + port + "!\n" + process.env.MESSAGE,
+    ),
+  );
+});
+
+const createUsersWithMessages = async () => {
+  await models.User.create(
+    {
+      username: "rwieruch",
+      email: "rwieruch@email.com",
+      messages: [
+        {
+          text: "Published the Road to learn React",
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
+
+  await models.User.create(
+    {
+      username: "ddavids",
+      email: "ddavids@email.com",
+      messages: [
+        {
+          text: "Happy to release ...",
+        },
+        {
+          text: "Published a complete ...",
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
+};
 
 export default app;
